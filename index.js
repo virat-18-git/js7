720595bab2697677a8b2a3bbe611aa7911f34a7a
virat-18-git <ksprddy6@gmail.com>
2024-12-25 19:47:44 +0530

    for loop

---
+++ index.js
@@ -0,0 +1,87 @@
+
+for (i=1;i<=20;i++)
+    for (i=10;i>=1;i--)
+{
+    // console.log(`5 X ${i} = ${i*5}`)
+}
+
+let str="welcome";
+for(i=0;i<=6;i++)
+{
+    console.log(str[i])
+}
+
+// let str="welcome to the new world";
+// for(i=0;i<str.length;i++)
+// {
+//     console.log(str[i])
+// }
+
+// let arr=[5,7,8,6,88,346,964,2156]
+// for(i=0;i<arr.length;i++)
+//     for(i=0;i<=arr.length-1;i++)
+// {
+//     console.log(arr[i])
+// }
+
+// let str1="welcome"
+// for(i=str1.length;i>=0;i--)
+// {
+//     console.log(str1[i])
+// }
+
+// let str2="welcome"
+// let op=""
+// for(i=str2.length-1;i>=0;i--)
+// {
+  
+// op+=str2[i]
+   
+// }
+// console.log(op)
+
+// let str2="shiva prasad"
+// let op=""+
+// for(i=0;i<=str2.length-1;i++)
+// {
+  
+// op+=str2[i]
+// console.log(op)
+   
+// }
+
+// op=""
+// for(i=0;i<10;i++){
+//     op+="*"
+//     console.log(op)
+
+// }
+
+
+// let arr=["shiva","vamshi","virat","kohli","pawan","kalyaan"]
+// for(i=0;i<=5;i++)
+// {
+//     console.log(arr[i])
+// }
+
+// let arr=["shiva","vamshi","virat","kohli","pawan","kalyaan"]
+// for(i=0;i<=arr.length-1;i=i+2)
+// {
+//     console.log(arr[i])
+// }
+
+
+// let arr1=["shiva","vamshi","virat","kohli","pawan","kalyaan"]
+// for(i=0;i<=arr1.length-1;i=i+3)
+// {
+//     console.log(arr1[i])
+// }
+
+
+let arr=["shiva","vamshi","virat","kohli","pawan","kalyaan"]
+for(i=0;i<=arr.length-1;i++)
+{
+    console.log(arr[i][0])
+}
+
+
